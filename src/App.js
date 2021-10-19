0f26f354132092b523e9fea12542d0d624346eb1
spaceboy <chungisu@gmail.com>
2021-10-19 22:31:09 +0900

    delete workflow folder

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test updates
+          Hello! test updates.
         </p>
         <a
           className="App-link"
